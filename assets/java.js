468f79ac11e7fe5d1d4b979739dcc708379a3d0f
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-07-26 20:59:40 -0500

    added html layout, placeholders for a train, added my firebase config

---
+++ assets/java.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyA9dF84zLBGjhNOw6l-Pv479UKLVY-rSWo",
+    authDomain: "greatstart-e724e.firebaseapp.com",
+    databaseURL: "https://greatstart-e724e.firebaseio.com",
+    projectId: "greatstart-e724e",
+    storageBucket: "",
+    messagingSenderId: "797759222663",
+    appId: "1:797759222663:web:a5d379802b8c0e75"
+  };
+ 
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+  
+  
