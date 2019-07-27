7b4fb8aedcb6a15d39d981e637e4686f53bc5db2
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-07-26 23:06:04 -0500

    saved train inputs in my database, converted military date to pm-am format with moment js, empted train inputs

--- assets/java.js
+++ assets/java.js
@@ -15,5 +15,45 @@
  
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
+
+
+
+  // 2. Button for adding trains
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    // Grabs user input
+    var trName = $("#train-name-input").val().trim();
+    var trDestination = $("#destination-input").val().trim();
+    var trStart = moment($("#start-input").val().trim(), "HH:mm").format("hh:mm A");
+    var trFrequency = $("#frequency-input").val().trim();
+  
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+      name: trName,
+      destination: trDestination,
+      start: trStart,
+      frequency: trFrequency
+    };
+  
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+  
+    // Logs everything to console
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.start);
+    console.log(newTrain.frequency);
+  
+    alert("Train successfully added");
+  
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#start-input").val("");
+    $("#frequency-input").val("");
+  });
+  
+  
   
   
